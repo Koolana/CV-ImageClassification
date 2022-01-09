3e7ad1396aeb3033bf80a63c3a3ddbf51aa3a673
Nikolay Andreychik <kolya.andreychik@gmail.com>
2022-01-10 01:12:42 +0300

    Added jupyter notebook and modified model

--- model.py
+++ model.py
@@ -5,8 +5,9 @@ class ClassificationModel(nn.Module):
     def __init__(self):
         super(ClassificationModel, self).__init__()
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout()
 
-        self.conv1 = nn.Conv2d(3, 48, 11, 4, 0)
+        self.conv1 = nn.Conv2d(3, 48, 5, 2, 0)
         self.pool1 = nn.MaxPool2d(3, 2)
 
         self.conv2 = nn.Conv2d(48, 128, 5, 1, 2)
@@ -17,9 +18,9 @@ class ClassificationModel(nn.Module):
         self.conv5 = nn.Conv2d(256, 128, 3, 1, 1)
         self.pool3 = nn.MaxPool2d(3, 2)
 
-        self.fc1 = nn.Linear(128 * 6 * 6, 256)  # 256px, 61 - зависимость от изображения и MaxPool2d (при 32px - 5)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 4)
+        self.fc1 = nn.Linear(128 * 14 * 14, 512)  # 256px, 61 - зависимость от изображения и MaxPool2d (при 32px - 5)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 4)
 
         print('Model created!')
 
@@ -44,14 +45,16 @@ class ClassificationModel(nn.Module):
         x = self.pool3(x)
         # print(x.size())
 
-        x = x.view(-1, 128 * 6 * 6)  # 256px, 61 - зависимость от изображения MaxPool2d (при 32px - 5)
+        x = x.view(-1, 128 * 14 * 14)  # 256px, 61 - зависимость от изображения MaxPool2d (при 32px - 5)
         # print(x.size())
 
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout(x)
 
         x = self.fc2(x)
         x = self.relu(x)
+        x = self.dropout(x)
 
         x = self.fc3(x)
         # print(x.size())
