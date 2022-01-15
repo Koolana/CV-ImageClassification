5f759af2baafa813ba8d6b6795bdb11a31364f5a
Nikolay Andreychik <kolya.andreychik@gmail.com>
2022-01-16 02:41:58 +0300

    Modified main file

--- main.py
+++ main.py
@@ -42,23 +42,17 @@ if __name__ == "__main__":
 	train_features, train_labels = next(iter(data_loader_train))
 	train_features = dataset.getImgsTensors(train_features)
 
-	print(f"Feature batch shape: {train_features.size()}")
-	print(f"Labels batch shape: {train_labels.size()}")
-
 	plt.title('Labels: ' + ', '.join([dataset.getName(i) for i in train_labels]))
 	gridImgs = torchvision.utils.make_grid(train_features)
 	# оставляем изображение в исходной цветовой гамме (три канала - цветное изображение)
 	plt.imshow(cv2.cvtColor(gridImgs.permute(1, 2, 0).numpy() / 255, cv2.COLOR_BGR2RGB))
-	# plt.show()
+	plt.show()
 
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	print(device)
+	print("Used device:", device)
 
 	model = ClassificationModel().to(device)
-	print(model)
 	torchsummary.summary(model, (3, 128, 128))
-
-	# optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 	trainer = Trainer(model, data_loader_train, data_loader_test, data_loader_val,
 					  device, getTensorsFunc=dataset.getImgsTensors, tqdm=tq.tqdm)
@@ -66,33 +60,49 @@ if __name__ == "__main__":
 	best_loss = np.inf
 	best_accuracy = 0.0
 
-	for epoch in range(30):
+	for epoch in range(1):
 		epoch_loss, _ = trainer.train(epoch + 1)
 
 		print('[%d] Train loss: %.10f %.10f' % (epoch + 1, epoch_loss, epoch_loss / len(data_loader_train)))
 
+		####################################################
+
 		epoch_loss, accuracyDict = trainer.test(epoch + 1)
 
 		print('[%d] Test loss: %.10f %.10f' % (epoch + 1, epoch_loss, epoch_loss / len(data_loader_test)))
-		print('[%d] Test accuracy:' % (epoch + 1))
-
-		print(*[f'\t{dataset.getName(i)}: {accuracyDict[i]:.2f}' for i in accuracyDict.keys()], sep='\n')
 
 		accuracy = sum([accuracyDict[i] for i in accuracyDict.keys()]) / len(accuracyDict.keys())
+		print('[%d] Test average accuracy: %.2f' % (epoch + 1, accuracy))
+
+		print('[%d] Test accuracy in classes:' % (epoch + 1))
+		print(*[f'\t{dataset.getName(i)}: {accuracyDict[i]:.2f}' for i in accuracyDict.keys()], sep='\n')
+
+		####################################################
 
 		if best_accuracy < accuracy:
 			print('New best model with average accuracy: ', accuracy)
 			best_accuracy = accuracy
 			best_model = pickle.loads(pickle.dumps(model))
 
-		print(10 * '-')
+		print(40 * '-')
 
 	print('Тренировка завершена, наилучшая средняя точность: ', best_accuracy)
 
-	print('Проверка наилучшей модели')
+	####################################################
+
+	print('\nПроверка наилучшей модели')
+
+	accuracyDict = trainer.validation(best_model)
+
+	test_accuracy = sum([accuracyDict[i] for i in accuracyDict.keys()]) / len(accuracyDict.keys())
+	print('Validation average accuracy: %.2f' % (test_accuracy))
+	print('Accuracy in classes:')
+	print(*[f'\t{dataset.getName(i)}: {accuracyDict[i]:.2f}' for i in accuracyDict.keys()], sep='\n')
+
+	####################################################
 
 	# Вывод на экран изображение и label (метку).
-	test_features, test_labels = next(iter(data_loader_test))
+	test_features, test_labels = next(iter(data_loader_val))
 	test_labels = test_labels.to(device)
 	test_features = dataset.getImgsTensors(test_features).to(device)
 
